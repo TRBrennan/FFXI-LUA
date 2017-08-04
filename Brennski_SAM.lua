e4d7afb793c8e546eaf2ceed4b1a488f0b073550
TRBrennan <tom.ronald.brennan@gmail.com>
2017-08-04 11:26:30 +0100

    Add files via upload

---
+++ Brennski_SAM.lua
@@ -0,0 +1,376 @@
+function get_sets()
+ 
+        send_command('bind f9 gs c toggle TP set')
+        send_command('bind f10 gs c toggle Idle set')
+		send_command('bind f11 gs c toggle Weapons')
+         function file_unload()
+    
+        send_command('unbind ^f9')
+        send_command('unbind ^f10')
+		send_command('unbind ^f11')
+
+        send_command('unbind !f9')
+        send_command('unbind !f10')
+		send_command('unbind !f11')
+		
+        send_command('unbind f9')
+        send_command('unbind f10')
+		send_command('unbind f11')
+ 
+        end    
+		
+		--Weapons--
+		
+		sets.Weapons = {}
+		
+		sets.Weapons.Index = {'GS', 'GA', 'Polearm','AxeShield', 'SwordShield', 'ClubShield', 'DaggerShield'}
+		Weapons_ind = 1
+		
+		sets.Weapons.GS = {main ="Ragnarok", sub ="Utu Grip"}
+		
+		sets.Weapons.GA = {main ="Chango", sub ="Utu Grip"}
+		
+		sets.Weapons.Polearm = {main ="Blurred Lance", sub ="Utu Grip"}
+		
+		sets.Weapons.AxeShield = {main ="Purgation", sub ="Blurred Shield"}
+		
+		sets.Weapons.SwordShield = {main ="Tanmogayi +1", sub ="Blurred Shield"}
+		
+		sets.Weapons.ClubShield = {main ="Loxotic Mace", sub ="Blurred Shield"}
+		
+		sets.Weapons.ClubShield = {main ="Ternion Dagger +1", sub ="Blurred Shield"}
+               
+        --Idle Sets--  
+        sets.Idle = {}
+       
+        sets.Idle.index = {'Standard','DT'}
+        Idle_ind = 1                  
+       
+        sets.Idle.Standard = {ammo="Staunch Tathlum", 
+							head="Rao kabuto",
+							body="Hizamaru haramaki +1",
+							hands="Rao kote",
+							legs={ name="Herculean Trousers", augments={'"Mag.Atk.Bns."+25','Phys. dmg. taken -5%','INT+6','Mag. Acc.+9',}},
+							feet="Rao sune-ate",
+							neck="Sanctity necklace",
+							waist="Flume Belt +1",
+							left_ear="Infused Earring", right_ear="Genmei Earring",   
+							right_ring="Vocane Ring",
+							left_ring="Defending Ring",
+							back="Moonbeam Cape",
+							}
+                                                 
+        sets.Idle.DT = { ammo="Staunch Tathlum", 
+							head="Rao kabuto",
+							body="Emet Harness +1",
+							hands="Rao kote",
+							legs={ name="Herculean Trousers", augments={'"Mag.Atk.Bns."+25','Phys. dmg. taken -5%','INT+6','Mag. Acc.+9',}},
+							feet="Rao sune-ate",
+							neck="Loricate Torque +1",
+							waist="Flume Belt +1",
+							left_ear="Infused Earring", right_ear="Genmei Earring",   
+							right_ring="Vocane Ring",
+							left_ring="Defending Ring",
+							back="Moonbeam Cape",
+							}
+										
+							
+        --TP Sets--
+        sets.TP = {}
+ 
+           sets.TP.index = {'Standard', 'AccuracyMid', 'AccuracyFull', 'DT', 'DTAccuracy'}
+                --1=Standard, 2 = AccuracyMid, 3=AccuracyFull, 4=DT, 5=DTAccuracy--
+				
+                TP_ind = 1
+				sets.TP.Standard = {ammo="Ginsen",
+                                    head="Flam. Zucchetto +1", neck="Lissome necklace", ear1="Cessance Earring",ear2="Telos Earring",
+                                    body={ name="Valorous Mail", augments={'Accuracy+21 Attack+21','STR+10','Accuracy+15','Attack+4',}},hands="Flam. Manopolas +1",
+									ring1="Niqmaddu Ring",ring2="Ilabrat Ring",
+                                     back="Lupine Cape",waist="Ioskeha Belt",
+									 legs={ name="Valor. Hose", augments={'Accuracy+23 Attack+23','"Dbl.Atk."+3','VIT+4','Attack+5',}},
+									feet={ name="Valorous Greaves", augments={'Accuracy+25 Attack+25','"Dbl.Atk."+4','CHR+8','Attack+6',}},}
+                                                       
+                                                       
+                sets.TP.AccuracyMid = {ammo="Seethng bomblet +1",
+                                        head={ name="Valorous Mask", augments={'Accuracy+30','"Store TP"+4','AGI+10','Attack+13',}}, 
+										neck="Lissome necklace", ear1="Cessance Earring",ear2="Telos Earring",
+										body={ name="Valorous Mail", augments={'Accuracy+21 Attack+21','STR+10','Accuracy+15','Attack+4',}},hands="Flam. Manopolas +1",ring1="Niqmaddu Ring",ring2="Regal Ring",
+                                         back="Lupine Cape",
+										 waist="Ioskeha Belt",legs={ name="Valor. Hose", augments={'Accuracy+23 Attack+23','"Dbl.Atk."+3','VIT+4','Attack+5',}},
+										feet={ name="Valorous Greaves", augments={'Accuracy+25 Attack+25','"Dbl.Atk."+4','CHR+8','Attack+6',}},}
+       
+                sets.TP.AccuracyFull = {ammo="Seething Bomblet +1",
+                                       head={ name="Valorous Mask", augments={'Accuracy+30','"Store TP"+4','AGI+10','Attack+13',}},
+										neck="Sanctity Necklace", ear1="Dignitary's Earring", ear2="Telos Earring",
+                                        body={ name="Valorous Mail", augments={'Accuracy+21 Attack+21','STR+10','Accuracy+15','Attack+4',}},
+										hands="Flam. Manopolas +1",ring1="Cacoethic Ring",ring2="Chirich Ring",
+                                        back="Lupine Cape",
+										waist="Ioskeha Belt",
+										legs={ name="Valor. Hose", augments={'Accuracy+23 Attack+23','"Dbl.Atk."+3','VIT+4','Attack+5',}},
+										feet={ name="Valorous Greaves", augments={'Accuracy+25 Attack+25','"Dbl.Atk."+4','CHR+8','Attack+6',}},}
+                                                       
+                sets.TP.DT = {ammo="Staunch Tathlum",
+                              head={ name="Valorous Mask", augments={'Accuracy+30','"Store TP"+4','AGI+10','Attack+13',}}, 
+							  neck="Loricate Torque +1", ear1="Cessance Earring",ear2="Telos Earring",
+                              body="Emet Harness +1",hands="Sulev. Gauntlets +2",ring1="Vocane Ring",ring2="Defending Ring",
+                               back="Lupine Cape",
+							   waist="Ioskeha Belt",legs="Sulevia's Cuisses +2",feet="Sulevia's Leggings +2"}
+                                 
+                sets.TP.DTAccuracy = {ammo="Ginsen",
+                                      head="Sulevia's Mask +2",neck="Lissome Necklace", ear2="Telos Earring",ear1="Odnowa Earring +1",    
+									  body="Emet Harness +1",hands="Sulev. Gauntlets +2",ring1="Niqmaddu Ring",ring2="Chirich Ring",
+                                       back="Lupine Cape",
+									   waist="Ioskeha Belt",legs="Sulevia's Cuisses +2",feet="Sulevia's Leggings +2"}
+                                                         
+									
+       --Weaponskill Sets--
+        sets.WS = {}
+		
+		sets.OneHit = {}
+		
+		sets.OneHit.Attack = {ammo="Knobkierrie",
+							head={ name="Valorous Mask", augments={'Accuracy+18','Weapon skill damage +3%','STR+4','Attack+11',}},
+							body={ name="Valorous Mail", augments={'Accuracy+25','Weapon skill damage +4%','DEX+8','Attack+5',}},
+							hands={ name="Valorous Mitts", augments={'Accuracy+17 Attack+17','Weapon skill damage +3%','VIT+9','Accuracy+3','Attack+12',}},
+							legs="Hiza. Hizayoroi +2",
+							feet="Hiza. Sune-Ate +1",
+							neck="Caro Necklace",
+							waist="Grunfeld Rope",
+							left_ear="Ishvara Earring",
+							right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
+							left_ring="Regal Ring",
+							right_ring="Niqmaddu Ring",
+							back="Lupine Cape"}
+		
+		sets.MultiHit = {}
+		
+		sets.MultiHit.Attack = { ammo="Knobkierrie",
+							head={ name="Valorous Mask", augments={'Accuracy+18','Weapon skill damage +3%','STR+4','Attack+11',}},
+							body={ name="Valorous Mail", augments={'Accuracy+25','Weapon skill damage +4%','DEX+8','Attack+5',}},
+							hands={ name="Valorous Mitts", augments={'Accuracy+17 Attack+17','Weapon skill damage +3%','VIT+9','Accuracy+3','Attack+12',}},
+							legs="Hiza. Hizayoroi +2",
+							feet={ name="Valorous Greaves", augments={'Accuracy+25 Attack+25','"Dbl.Atk."+4','CHR+8','Attack+6',}},
+							neck="Fotia Gorget",
+							waist="Fotia Belt",
+							left_ear="Telos Earring",
+							right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
+							left_ring="Regal Ring",
+							right_ring="Niqmaddu Ring",
+							back="Lupine Cape"}
+		
+		sets.Hyrbid = {}
+		
+		sets.Hybrid.Attack = {ammo="Knobkierrie",
+							head={ name="Valorous Mask", augments={'Accuracy+18','Weapon skill damage +3%','STR+4','Attack+11',}},
+							body="Found. Breastplate",
+							hands={ name="Valorous Mitts", augments={'Accuracy+17 Attack+17','Weapon skill damage +3%','VIT+9','Accuracy+3','Attack+12',}},
+							legs="Hiza. Hizayoroi +2",
+							feet="Hiza. Sune-Ate +1",
+							neck="Fotia Gorget",
+							waist="Fotia Belt",
+							left_ear="Friomisi earring",
+							right_ear={ name="Moonshade Earring", augments={'Accuracy+4','TP Bonus +25',}},
+							left_ring="Regal Ring",
+							right_ring="Niqmaddu Ring",
+							back="Izdubar Mantle"}
+       
+        --Ninja Magic Sets--
+        sets.NINMagic = {}
+       				
+	    sets.NINMagic.Utsusemi ={
+                              head="Dampening Tam",neck="Loricate Torque +1", ear1="Brutal Earring",ear2="Cessance Earring",
+                              body="Emet harness +1",hands="Sulev. Gauntlets +2",ring1="Vocane Ring",ring2="Petrov Ring",
+                              back="Moonbeam Cape",waist="Flume belt +1",legs="Herculean Trousers",feet="Valorous Greaves"}
+ 
+                                              
+        --Utility Sets--
+        sets.Utility = {}
+       
+        sets.Utility.Weather = {waist="Hachirin-no-obi",back="Twilight Cape"}
+ 
+        sets.Utility.MB = {head="Herculean Helm",body="Amalric Doublet",ear1="Static Earring",ring1="Locus Ring",ring2="Mujin Band"} 
+       
+        sets.Utility.Stoneskin = {head="Haruspex hat",neck="Stone Gorget",ear1="Loquac. earring",ear2="Earthcry earring",
+                                                          body="Assim. jubbah +1",hands="Stone Mufflers",ring1="Rahab ring",
+                                                          back="Swith cape",waist="Siegel sash",legs="Haven hose",feet="Valorous Greaves"}
+                                                         
+        sets.Utility.Phalanx = {head="Haruspex hat",neck="Colossus's torque",ear1="Loquac. earring",ear2="Augment. earring",
+                                                    body="Assim. jubbah +1",hands="Ayao's gages",ring1="Rahab ring",
+                                                        back="Swith cape",waist="Pythia sash +1",legs="Portent pants",feet="Valorous Greaves"}
+                                                       
+        sets.Utility.Steps = {ammo="Ginsen",
+							head="Boii Mask +1", body={ name="Valorous Mail", augments={'Accuracy+21 Attack+21','STR+10','Accuracy+15','Attack+4',}}, hands="Rawhide Gloves",legs=="Odyssean Cuisses",
+							feet="Valorous Greaves", neck="Subtlety Spec.",waist="Chaac Belt", left_ear="Heartseeker Earring",
+							right_ear="Dignitary's Earring", left_ring="Yacuruna Ring", right_ring="Cacoethic Ring",   back="Lupine Cape",}
+                                                 
+		sets.Utility.Doomed = {waist="Gishdubar Sash", ring1 ="Saida Ring"}
+		
+		sets.Utility.Enmity = {ammo="Sapience Orb",
+								ear2 ="Friomisi Earring", 
+								back ="Weard Mantle",
+								waist ="Trance belt",
+								ring2="Supershear Ring",
+								neck = "Unmoving Collar",
+								hands="Souveran Handschuhs",
+								feet="Souveran Schuhs",
+								body="Souveran Cuirass",
+								ring1="Petrov Ring",
+								legs="Souveran Diechlings +1",
+								head="Souveran Schaller"}
+		
+		sets.Utility.Sleeping = {neck="Opo-Opo Necklace"}
+        --Job Ability Sets--
+       
+        sets.JA = {}
+               
+		sets.JA.Meditate ={back = "Cichol's Mantle",feet = "Agoge Calligae", body = "Pumm. Lorica +3"}
+
+		sets.JA.Hasso = { body = "Agoge Lorcia", head ="Pummeler's Mask +1"}
+		
+		sets.JA.BladeBash= {head = "Agoge Mask"}		
+		
+		sets.JA.Sekkanoki = {hands = "Warrior's Muffler +2"}
+		
+		sets.JA.Seigan ={feet = "Ravager's Calligae +1"}
+		
+		sets.JA.BloodRage ={body ="Ravager's Lorica +2"}
+		
+		sets.JA.Tomahawk = {ammo = "Throwing Tomahawk",
+						feet = "Agoge Calligae", waist ="Chaac Belt", 
+						head={ name="Valorous Mask", augments={'INT+6','"Dbl.Atk."+1','"Treasure Hunter"+1','Accuracy+6 Attack+6','Mag. Acc.+16 "Mag.Atk.Bns."+16',}}}
+
+		sets.JA.Restraint ={}
+        --Precast Sets--
+        sets.precast = {}
+       
+        sets.precast.FC = {}
+       
+       sets.precast.FC.Standard = { ammo="Sapience Orb",
+							head="Carmine Mask +1", 
+							body="Odyssean Chestplate",
+							hands="Leyline Gloves",
+							legs={ name="Odyssean Cuisses", augments={'Mag. Acc.+15 "Mag.Atk.Bns."+15','"Fast Cast"+3','INT+1','Mag. Acc.+9','"Mag.Atk.Bns."+3',}},
+						    feet="Odyssean Greaves",
+							neck="Voltsurge Torque",
+							waist="Witful Belt",
+							left_ear="Etiolation Earring",
+						    right_ear="Loquac. Earring",
+							left_ring="Rahab Ring",
+							right_ring="Kishar Ring", 
+							back="Swith Cape"}
+       
+end
+ 
+  
+function precast(spell)
+        if spell.type == 'Magic' then
+                equip(sets.precast.FC.Standard)
+        elseif spell.english == 'Tachi: Fudo' or spell.english == 'Tachi: Kasha' or spell.english == 'Tachi: Shoha' then
+                equip(sets.OneHit.Attack)
+        elseif spell.english == "Tachi: Jinpu" then
+                equip(sets.Hybrid.Attack)
+        elseif spell.english == 'Stardiver' or spell.english == 'Tachi: Rana' then
+			equip(sets.MultiHit.Attack)
+		elseif spell.english =='Meditate' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.Meditate))
+		elseif spell.english =='Hasso' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.Hasso))
+		elseif spell.english =='Blade Bash' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.Blade Bash))
+		elseif spell.english =='Tomahawk' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.Tomahawk))
+		elseif spell.english =='Bloodrage' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.BloodRage))
+		elseif spell.english =='Sekkanoki' then
+			equip(set_combine(sets.Utility.Enmity,sets.JA.Sekkanoki))
+		elseif spell.english =='Provoke' then
+			equip(sets.Utility.Enmity)
+		elseif spell.english == 'Box Step' then
+                equip(sets.Utility.Steps)
+        elseif spell.type == 'Weapon Skill' then
+			equip(sets.OneHit.Attack)
+		end
+       
+end
+       
+function midcast(spell,act)
+		if spell.skill =='Ninjutsu' then
+			equip(sets.NINMagic.Nuke)
+			 if spell.element == world.day_element or spell.element == world.weather_element then
+                              equip(set_combine(sets.NINMagic.Nuke,sets.Utility.Weather))
+                        end
+		end
+		
+	    if spell.english == 'Utsusemi: Ichi' then
+                equip(sets.NINMagic.Utsusemi)
+                        if buffactive['Copy Image (3)'] then
+                                send_command('@wait 0.3; input //cancel Copy Image*')
+                        end
+                        if buffactive['Copy Image (2)'] then
+                                send_command('@wait 0.3; input //cancel Copy Image*')
+                        end
+                        if buffactive['Copy Image (1)'] then
+                                send_command('@wait 0.3; input //cancel Copy Image*')
+                        end
+                        if buffactive['Copy Image'] then
+                                send_command('@wait 0.3; input //cancel Copy Image*')
+                        end
+        end
+ 
+        if spell.english == 'Utsusemi: Ni'  or spell.english == 'Utsusemi: San' then
+                equip(sets.NINMagic.Utsusemi)
+        end
+end	
+ 
+function aftercast(spell)
+        if player.status == 'Engaged' then
+                equip(sets.TP[sets.TP.index[TP_ind]])
+				if buffactive['doom'] then
+					equip(sets.Utility.Doom)
+				end
+				if buffactive['terror'] or buffactive['stun'] or buffactive['sleep']	then 
+					equip(sets.TP.DT)
+				end
+
+		else
+                equip(sets.Idle[sets.Idle.index[Idle_ind]])					
+					if buffactive['doom'] then
+					equip(sets.Utility.Doom)
+				end
+				if buffactive['terror'] or buffactive['stun'] or buffactive['sleep']	then 
+					equip(sets.TP.DT)
+				end
+
+        end
+end
+
+ 
+function status_change(new,old)
+        if player.status == 'Engaged' then
+                equip(sets.TP[sets.TP.index[TP_ind]])
+        else
+                equip(sets.Idle[sets.Idle.index[Idle_ind]])					
+        end
+end 
+function self_command(command)
+        if command == 'toggle TP set' then
+                TP_ind = TP_ind +1
+                if TP_ind > #sets.TP.index then TP_ind = 1 end
+                send_command('@input /echo <----- TP Set changed to '..sets.TP.index[TP_ind]..' ----->')
+                equip(sets.TP[sets.TP.index[TP_ind]])
+        elseif command == 'toggle Idle set' then
+                Idle_ind = Idle_ind +1
+                if Idle_ind > #sets.Idle.index then Idle_ind = 1 end
+                send_command('@input /echo <----- Idle Set changed to '..sets.Idle.index[Idle_ind]..' ----->')
+                equip(sets.Idle[sets.Idle.index[Idle_ind]])
+		elseif command == 'toggle Weapons' then
+                Weapons_ind = Weapons_ind +1
+               if Weapons_ind > #sets.Weapons.Index then Weapons_ind = 1 end
+                send_command('@input /echo <----- Weapons Set changed to '..sets.Weapons.Index[Weapons_ind]..' ----->')
+                equip(sets.Weapons[sets.Weapons.Index[Weapons_ind]])
+        elseif command == 'equip TP set' then
+                equip(sets.TP[sets.TP.index[TP_ind]])
+        elseif command == 'equip Idle set' then
+                equip(sets.Idle[sets.Idle.index[Idle_ind]])
+        end
+end
